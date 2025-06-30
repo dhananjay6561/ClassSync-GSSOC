9a17366dab9f14de03ae29fa1ae01f3ab31fc3c7
Dhananjay Aggarwal <dhananjayaggarwal6561@gmail.com>
2025-06-30 11:44:06 +0530

    updated vercel.json and vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,31 @@
-import path from "path"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+// import path from "path"
+// import react from "@vitejs/plugin-react-swc"
+// import { defineConfig } from "vite"
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: "./",
+//   plugins: [react()],  
+// })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],  
+  plugins: [
+    react(),
+    tailwindcss({
+      theme: {
+        
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
